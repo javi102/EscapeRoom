35549c8ff0f56b21437c42273c5880e1b93d987d
javi <javier.gomez.casas022122@alumnosfesac.es>
2022-05-17 20:16:06 +0200

    .

---
+++ codigo.js
@@ -0,0 +1,11 @@
+var segundos=50;
+var pagina="index3.html";
+function cambiarTexto(){
+    document.getElementById("texto").innerHTML = "Teneis que superar las siguientes pruebas y obtener<br> el codigo secréto para poder <br> escapar del recinto.Para poder tener una mejor<br> experiencia del juego se recomienda el uso del sonido. <br><br>Haz click en avanzar para comenzar" ;
+    
+}
+
+
+
+
+
---
+++ fnaf.js
@@ -0,0 +1,6 @@
+alert("Bienvenidos a la primera prueba");
+alert("Os voy a dar una pista: A veces con dar un solo click....")
+
+function AbriVentana(){
+    window.open("https://mercadosevilla.com/project/pescaderia-loli/", "Diseño web", "width=6200, height=2100");
+}
